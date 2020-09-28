a18b356e20d02ebff95daac9ab8ada00b8620719
Ricky Garcia <rickyg08@msn.com>
2020-09-28 15:05:39 -0600

    Add @override with toast.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/MainActivity.java
@@ -1,13 +1,30 @@
 package edu.cnm.deepdive.codebreaker;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  private ListView guessList;
+  private EditText guess;
+  private Button submit;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    guessList = findViewById(R.id.guess_list);
+    guess = findViewById(R.id.guess);
+    submit = findViewById(R.id.submit);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Toast.makeText(this, "So far, so good!", Toast.LENGTH_LONG).show();
   }
 }
