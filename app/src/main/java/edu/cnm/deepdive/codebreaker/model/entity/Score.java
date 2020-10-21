94924c3eb161148623cf66e6d77e1884f9e04f12
Ricky Garcia <rickyg08@msn.com>
2020-10-21 12:03:16 -0600

    Repository in progress.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Score.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Score.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.codebreaker.model.entity;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -12,8 +13,9 @@ public class Score {
   @ColumnInfo(name = "game_id")
   private long id;
 
+  @NonNull
   @ColumnInfo(index = true)
-  private Date timestamp;
+  private Date timestamp = new Date();
 
   @ColumnInfo(name = "guess_count")
   private int guessCount;
